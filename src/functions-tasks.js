56a12bc8b7314960e4df573e12fa4d1e69504c90
yuyuu-8 <sishichulu@gmail.com>
2025-03-28 23:46:53 +0300

    feat: add implement 4 functions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -50,8 +50,8 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.map((func) => func.length);
 }
 
 /**
@@ -70,8 +70,10 @@ function getArgumentsCount(/* funcs */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function f(num) {
+    return num ** exponent;
+  };
 }
 
 /**
@@ -87,8 +89,32 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...coefficients) {
+  if (coefficients.length === 0) {
+    return null;
+  }
+  return function f(x) {
+    let result = '';
+    let value = 0;
+    coefficients.forEach((coef, index) => {
+      const power = coefficients.length - index - 1;
+      if (coef !== 0) {
+        if (result) {
+          result += coef > 0 ? ` + ${coef}` : ` - ${-coef}`;
+        } else {
+          result += coef;
+        }
+
+        if (power !== 0) {
+          result += `*y^${power}`;
+        }
+
+        value += coef * x ** power;
+      }
+    });
+
+    return value;
+  };
 }
 
 /**
